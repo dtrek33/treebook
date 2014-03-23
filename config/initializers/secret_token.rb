f127824384332b2645e3942f730e80db51ebba83
Tameem Assaf <tameem.assaf@gmail.com>
2014-03-23 19:34:09 -0400

    Add user friendships and associated tests + re Baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '1033fe7685836b1ae037af1a28fca8cea03c9aa6bbacfb013b6ad770185e54b11a09f0a221168a62c813470239dc26f724df982af598ca4ac31efb01c9850bd4'
